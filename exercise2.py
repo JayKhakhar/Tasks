45887908749587e10acbcc1b7107e8de21c4aae9
JayKhakhar <156770724+JayKhakhar@users.noreply.github.com>
2024-01-18 16:45:28 +0530

    Update exercise2.py

--- exercise2.py
+++ exercise2.py
@@ -14,14 +14,6 @@ class Category:
         else:
             self.display_name = self.name
 
-    def add_products_recursive(self, products):
-        for product in products:
-            self.add_product(product)
-
-        for child_category in self.products:
-            if isinstance(child_category, Category):
-                child_category.add_products_recursive(products)
-
     def display_info(self):
         print(f"\nCategory: {self.display_name}\nCode: {self.code}\nProducts:")
         if not self.products:
@@ -33,6 +25,16 @@ class Category:
     def get_category_chain(self):
         return [self.display_name] + (self.parent.get_category_chain() if self.parent else [])
 
+    def add_products_recursive(self, products):
+        def add_to_category(category, products):
+            for product in products:
+                category.add_product(product)
+
+            for child_category in category.products:
+                if isinstance(child_category, Category):
+                    add_to_category(child_category, products)
+
+        add_to_category(self, products)
 
 class Product:
     def __init__(self, name, code):
@@ -40,7 +42,6 @@ class Product:
 
     def display_info(self):
         print(f"  Product: {self.name}\n  Code: {self.code}")
-
 
 # Creating category objects
 vehicle_category = Category("Vehicle", "V001")
@@ -60,12 +61,18 @@ products_petrol = [Product(f"Product{num}", f"PP{num + 1:03}") for num in range(
 products_diesel = [Product(f"Product{num}", f"DP{num + 1:03}") for num in range(3)]
 products_bike = [Product(f"Product{num}", f"BP{num + 1:03}") for num in range(3)]
 
+# List of tuples containing a category and its corresponding products
+category_product_tuples = [
+    (vehicle_category, products_vehicle),
+    (car_category, products_car),
+    (petrol_category, products_petrol),
+    (diesel_category, products_diesel),
+    (bike_category, products_bike),
+]
+
 # Adding static products to respective categories using recursion
-vehicle_category.add_products_recursive(products_vehicle)
-car_category.add_products_recursive(products_car)
-petrol_category.add_products_recursive(products_petrol)
-diesel_category.add_products_recursive(products_diesel)
-bike_category.add_products_recursive(products_bike)
+for category, products in category_product_tuples:
+    category.add_products_recursive(products)
 
 # Displaying product list by category (group by category, order by category name)
 categories = [vehicle_category, car_category, petrol_category, diesel_category, bike_category]
